e9b623ad53b0ed059a6d61ed5c9f259cc3657d34
avinoorsinghkohli <avinoor19460@iiitd.ac.in>
2024-08-01 20:10:14 -0500

    JS Vs React

---
+++ abc.js
@@ -0,0 +1,18 @@
+// console.log("ABC")
+const student = {
+    fullName: "Avinoor",
+    age: 20,
+    cgpa: 8.2,
+    isPass: true,
+};
+// console.log(student.fullName)
+// console.log(student["fullName"]), values are muttable
+/*multi line comment*/
+// console.log("a+b = ", 5);
+let color;
+// if(mode === "dark-mode"){
+//     color = "black";
+// }
+// == means 5 = 5.0, === means 5!=5.0
+
+let num = prompt("Enter a number:");
