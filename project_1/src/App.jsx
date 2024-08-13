4e7b1acfcfab21a8b3331cbeced25e70c0390a9b
avinoorsinghkohli <avinoor19460@iiitd.ac.in>
2024-08-13 11:02:16 -0500

    Conditional Rendering

--- project_1/src/App.jsx
+++ project_1/src/App.jsx
@@ -2,13 +2,27 @@ import {CORE_CONCEPTS} from './data.js'
 import {useState} from 'react'
 import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcepts/CoreConcetps.jsx';
+import {EXAMPLES} from './data.js'
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please Click a button")
+  const [selectedTopic, setSelectedTopic] = useState()
+
   function handleSelect(selectedButton){
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
+  }
+  let tabSet = <p>Please Select a button</p>;
+  if(selectedTopic){
+    tabSet = <div id="tab-content">
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>
+      {EXAMPLES[selectedTopic].code}
+      </code>
+    </pre>
+  </div>
   }
   return (
     <div>
@@ -29,12 +43,23 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-          <TabButton onSelect={() => handleSelect("Components")}>Components</TabButton>
-          <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
-          <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
-          <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+          <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+          <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+          <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+          <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {/* {!selectedTopic && <p>Please Select a button</p>}
+          {selectedTopic && (<div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+        )} */}
+        {tabSet}
         </section>
         <h2>Time to get started!</h2>
       </main>
